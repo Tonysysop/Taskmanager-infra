768a9378e24748495c7a20a9a0b10212940d8166
Tonysysop <mrtonero4@gmail.com>
2025-03-24 21:57:49 +0100

    infra-setup

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.11.1"
+  # required_version = ">= 1.11.1"
   backend "s3" {
     bucket         = "mrtonero-statefile"
     key            = "terraform.tfstate"
